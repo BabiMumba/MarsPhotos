67d239b8e547085fe728ba7def9e6a57e020783a
babimumba <babimumba243@gmail.com>
2024-05-28 12:34:32 +0200

    finish

--- app/src/main/java/cd/bmduka/marsphoto/model/MarsPhoto.kt
+++ app/src/main/java/cd/bmduka/marsphoto/model/MarsPhoto.kt
@@ -4,9 +4,6 @@ package cd.bmduka.marsphoto.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-/**
- * This data class defines a Mars photo which includes an ID, and the image URL.
- */
 @Serializable
 data class MarsPhoto(
     val id: String,
